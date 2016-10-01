f7a7e4a669705d56a0684534df430b5bfe9114b7
Jonny Burger <jonathanburger11@gmail.com>
2016-10-01 15:34:43 +0200

    Add semicolons in test.js

--- test.js
+++ test.js
@@ -1,28 +1,27 @@
-var SpotifyWebHelper = require('./index')
+var SpotifyWebHelper = require('./index');
 
-var helper = SpotifyWebHelper()
+var helper = new SpotifyWebHelper();
 
-
-helper.player.on('ready', function() {
-	helper.player.on('play', function() {
+helper.player.on('ready', function () {
+	helper.player.on('play', function () {
 		console.log('play');
-	})
-	helper.player.on('pause', function() {
+	});
+	helper.player.on('pause', function () {
 		console.log('paused');
-	})
-	helper.player.on('end', function() {
+	});
+	helper.player.on('end', function () {
 		console.log('ended');
-	})
+	});
 	helper.player.on('track-change', function (track) {
 		console.log('track changed', track);
-	})
+	});
 	helper.player.on('error', function (error) {
 		console.error(error);
-	})
-	console.log(helper.status)
+	});
+	console.log(helper.status);
 	// 'status': {
 	//  	'track': ...,
 	//		'shuffle': ...,
 	//		'playing_position': ...
 	//  }
-})
+});
